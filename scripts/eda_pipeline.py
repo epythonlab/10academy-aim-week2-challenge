bda8ca3c2703dc1d8422b5f2914f747fc93797f1
Dejene Techane <dtechane6@gmail.com>
2024-09-05 18:56:12 -0400

    Calculated the basic metrices and univariate despersion with quantitative variables

--- scripts/eda_pipeline.py
+++ scripts/eda_pipeline.py
@@ -11,21 +11,23 @@ class EDA:
     def __init__(self, df):
         self.df = df
 
-    def compute_basic_metrics(self):
+    def compute_basic_metrics(self, variables):
         metrics = {}
         
         # Columns of interest
-        columns_of_interest = ['Dur. (ms)', 'Total UL (Bytes)', 'Total DL (Bytes)']
+        # columns_of_interest = ['Dur. (ms)', 'Total UL (Bytes)', 'Total DL (Bytes)']
         
-        for col in columns_of_interest:
-            metrics[col] = {
-                'mean': self.df[col].mean(),
-                'median': self.df[col].median(),
-                'std_dev': self.df[col].std(),
-                '90th_percentile': self.df[col].quantile(0.90),
-                'min': self.df[col].min(),
-                'max': self.df[col].max()
-            }
+        for var in variables:
+            metrics[var] = {
+                'Mean': self.df[var].mean(),
+                'Median': self.df[var].median(),
+                'Mode': self.df[var].mode().values[0],  # mode() returns a Series
+                'Standard Deviation': self.df[var].std(),
+                'Variance': self.df[var].var(),
+                'Range': self.df[var].max() - self.df[var].min(),
+                'IQR': self.df[var].quantile(0.75) - self.df[var].quantile(0.25)
+    }
+
         
         # Convert the metrics into a DataFrame for better presentation
         metrics_df = pd.DataFrame(metrics)
@@ -53,6 +55,8 @@ class EDA:
         }).reset_index()
 
         return decile_summary
+    
+    
 
     def plot_distribution(self, column):
         sns.histplot(self.df[column], kde=True)
@@ -64,7 +68,7 @@ class EDA:
         sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
         plt.title('Correlation Matrix')
         plt.show()
-
+        
     def univariate_analysis(self):
         # Example: Plot distribution of session duration
         plt.figure(figsize=(10, 6))
