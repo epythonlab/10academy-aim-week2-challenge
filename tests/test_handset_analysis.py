70b71d1c96a0bb433dda1b9d97a93fd8bb63fe56
Dejene Techane <dtechane6@gmail.com>
2024-09-05 15:26:41 -0400

    The error  fixed

--- tests/test_handset_analysis.py
+++ tests/test_handset_analysis.py
@@ -31,18 +31,6 @@ class TestHandsetAnalysis(unittest.TestCase):
         expected = pd.Series({
             'Apple': 4,
             'Samsung': 5
-        })
-        expected.index.name = 'Handset Manufacturer'
-        top_manufacturers.index.name = 'Handset Manufacturer'
-        expected.name = 'count'
-        top_manufacturers.name = 'count'
-        pd.testing.assert_series_equal(top_manufacturers.sort_index(), expected.sort_index())
-    
-    def test_top_manufacturers(self):
-        top_manufacturers = self.analysis.top_manufacturers(top_n=2)
-        expected = pd.Series({
-            'Apple': 4,
-            'Samsung': 5
         }).sort_index()  # Sort to ensure order is consistent
         
         expected.index.name = 'Handset Manufacturer'
@@ -51,3 +39,19 @@ class TestHandsetAnalysis(unittest.TestCase):
         top_manufacturers.name = 'count'
         
         pd.testing.assert_series_equal(top_manufacturers.sort_index(), expected)
+
+    
+    def test_top_handsets_per_manufacturer(self):
+        manufacturers = ['Apple', 'Samsung']
+        top_handsets = self.analysis.top_handsets_per_manufacturer(manufacturers, top_n_handsets=2)
+        
+        expected = {
+            'Apple': pd.Series({'iPhone 6S': 2, 'iPhone 6': 2}),
+            'Samsung': pd.Series({'Galaxy S8': 2, 'Galaxy A5': 1})
+        }
+        
+        for manufacturer in manufacturers:
+            expected[manufacturer].index.name = 'Handset Type'
+            top_handsets[manufacturer].index.name = 'Handset Type'
+            expected[manufacturer].name = 'count'
+            top_handsets[manufacturer]
